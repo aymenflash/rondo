95237c00c187de851a71448a1aa72c296999df2d
aymenflash <nasser.aymen12@gmail.com>
2016-04-18 11:10:24 +0200

    premier mise à jour

---
+++ app/src/main/java/com/rondo/aymen/rondo/MainRondo.java
@@ -0,0 +1,13 @@
+package com.rondo.aymen.rondo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainRondo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_rondo);
+    }
+}
